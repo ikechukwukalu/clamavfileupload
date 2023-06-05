ab5acb7ccdcad7c090b8d1a259c82f640be11c6c
Monitecture <100952206+monitecture@users.noreply.github.com>
2023-06-05 18:44:02 +0100

    Added Accesor for url field

--- src/Models/FileUpload.php
+++ src/Models/FileUpload.php
@@ -5,6 +5,8 @@ namespace Ikechukwukalu\Clamavfileupload\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Support\Facades\Storage;
 
 class FileUpload extends Model
 {
@@ -25,5 +27,13 @@ class FileUpload extends Model
         'folder',
         'hashed',
     ];
+
+    protected function url(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => $value,
+            set: fn (string $value) => Storage::url($value),
+        );
+    }
 
 }
